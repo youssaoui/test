242105adcb751309ce4bccf4478219b34439beda
youssaoui <youssaoui@gmail.com>
2021-05-04 16:30:33 +0000

    Update do.sh

--- do.sh
+++ do.sh
@@ -150,6 +150,7 @@ sudo adduser user chrome-remote-desktop
 printf '\nVisit http://remotedesktop.google.com/headless and Copy the command after authentication\n'
 read -p "Paste Command: " CRP
 su - user -c """$CRP"""
+service chrome-remote-desktop start
 
 printf "\n$c$b I hope everthing done correctly if mistakenly wrote wrong command or pin, Rerun the current box or run command 'su - user -c '<CRP Command Here>' $endc$enda\n" >&2
 printf "\n$c$b https://remotedesktop.google.com/access to access your VM, do not close browser tab to keep colab running ' $endc$enda\n" >&2
