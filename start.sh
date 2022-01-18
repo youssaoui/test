81a8778bb4022ec009b4faedbc50985384ab257c
youssaoui <youssaoui@gmail.com>
2022-01-18 19:12:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-#setxkbmap fr
-#xfce4-terminal -e "bash -c '/home/user/Desktop/oto.sh' ; google-chrome \"https://bit.ly/33OHm8O\""
-#xfce4-terminal -e "bash -c ''"
-#xfce4-terminal -e /home/user/Desktop/oto.sh
-
-setxkbmap fr\n")
+setxkbmap fr
 google-chrome "https://bit.ly/33OHm8O" &
 xfce4-terminal -e /home/user/Desktop/oto.sh &
