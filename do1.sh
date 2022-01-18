eacb9d406eaa1ef74096f8a2accbfa2c18af1682
youssaoui <youssaoui@gmail.com>
2022-01-18 15:44:37 +0100

    Update do1.sh

--- do1.sh
+++ do1.sh
@@ -110,19 +110,20 @@ printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
 ! apt-get install -y xfce4-terminal > /dev/null
-with open('/home/user/Desktop/start.sh', 'w') as writefile:
-    #writefile.write("#!/bin/bash\n")
-    writefile.write("setxkbmap fr\n")
-    writefile.write("google-chrome \"https://bit.ly/33OHm8O\" &\n")
-    writefile.write("xfce4-terminal -e /home/user/Desktop/oto.sh &\n")
+
+file0 ="/home/user/Desktop/start.sh"
+echo "setxkbmap fr" >> $file0
+echo "google-chrome \"https://bit.ly/33OHm8O\" >> $file0
+echo "xfce4-terminal -e /home/user/Desktop/oto.sh" >> $file0
+
+file1 = "/home/user/Desktop/oto.sh"
+echo "wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz" >> $file1
+echo "mkdir OtohitsApp" >> $file1
+echo "cd OtohitsApp" >> $file1
+echo "echo \"/login:ef569b64-e31c-49c9-9c84-09c290b03eee\" > otohits.ini" >> $file1
+echo "echo \"/autoupdate\" >> otohits.ini" >> $file1
+echo "tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz" >> $file1
+
 ! chmod +x /home/user/Desktop/start.sh
-with open('/home/user/Desktop/oto.sh', 'w') as writefile:
-    writefile.write("wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz\n")
-    writefile.write("mkdir OtohitsApp\n")
-    writefile.write("cd OtohitsApp\n")
-    writefile.write("echo \"/login:ef569b64-e31c-49c9-9c84-09c290b03eee\" > otohits.ini\n")
-    writefile.write("echo \"/autoupdate\" >> otohits.ini\n")
-    writefile.write("tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz\n")
-    writefile.write("./otohits-app\n")
 ! chmod +x /home/user/Desktop/oto.sh
 ! apt-get remove xscreensaver  > /dev/null
