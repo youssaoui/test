c3e5f5de54824595890f2c8663b5bb2dd7cde6c8
youssaoui <youssaoui@gmail.com>
2022-01-18 16:44:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-setxkbmap fr && google-chrome "https://bit.ly/33OHm8O" --no-default-browser-check
+setxkbmap fr
+google-chrome "https://bit.ly/33OHm8O" --no-default-browser-check
 xfce4-terminal -e /home/user/Desktop/oto.sh
 
