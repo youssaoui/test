b1041434f07fab37bf02e79f6dca38e2915902bc
youssaoui <youssaoui@gmail.com>
2022-01-30 21:25:29 +0100

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,7 +1,7 @@
 wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz
 mkdir OtohitsApp
 cd OtohitsApp
-echo \"/login:ef569b64-e31c-49c9-9c84-09c290b03eee\" > otohits.ini
-echo \"/autoupdate\" >> otohits.ini
+echo /login:ef569b64-e31c-49c9-9c84-09c290b03eee > otohits.ini
+echo /autoupdate >> otohits.ini
 tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
 ./otohits-app
